3c1eb62dad3e4bb028e4d2d538b5549482552eb8
darylfamenialph <56055452+darylfamenialph@users.noreply.github.com>
2023-06-04 12:43:53 +0800

    Initial Commit

---
+++ libs/services/src/lib/todo/sample-data.ts
@@ -0,0 +1,59 @@
+
+
+export const sampleData = [
+    {
+        _id: 'abcded',
+        title: 'sample1',
+        description: 'Sample 1',
+        status: 'active',
+        createdByUserID: '1',
+        createdAt: new Date('2023-06-04'),
+        updatedAt: new Date('2023-06-04')
+    },
+    {
+        _id: 'adfadfdf',
+        title: 'sample2',
+        description: 'Sample 2',
+        status: 'active',
+        createdByUserID: '1',
+        createdAt: new Date('2023-06-04'),
+        updatedAt: new Date('2023-06-04')
+    },
+    {
+        _id: 'adf32w2342d',
+        title: 'sample2',
+        description: 'Sample 2',
+        status: 'active',
+        createdByUserID: '1',
+        createdAt: new Date('2023-06-04'),
+        updatedAt: new Date('2023-06-04')
+    },
+    {
+        _id: '234sdfadfi0',
+        title: 'sample3',
+        description: 'Sample 3',
+        status: 'active',
+        createdByUserID: '1',
+        createdAt: new Date('2023-06-04'),
+        updatedAt: new Date('2023-06-04')
+    },
+    {
+        _id: '2343090a9df',
+        title: 'sample4',
+        description: 'Sample 4',
+        status: 'active',
+        createdByUserID: '1',
+        createdAt: new Date('2023-06-04'),
+        updatedAt: new Date('2023-06-04')
+    },
+    {
+        _id: '2343dfsd09342341s',
+        title: 'sample5',
+        description: 'Sample 5',
+        status: 'active',
+        createdByUserID: '1',
+        createdAt: new Date('2023-06-04'),
+        updatedAt: new Date('2023-06-04')
+    },
+    
+];
